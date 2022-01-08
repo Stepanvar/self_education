a868d51485c51d1b60ca5e73f7722670f879fafa
Clydas Curie <ccurie@ox-m3.msk.21-school.ru>
2022-01-08 14:48:48 +0300

    delete *.c files from the current directory

--- src/ex01.c
+++
@@ -1,6 +0,0 @@
-#include "stdio.h"
-int	ft_ex01(void)
-{
-	puts("ya slishu utrenniy kolokol\nOn besov draznit i zvonom nebo raskoloto\nYa l'ubil lish tebya");
-	return (0);
-}
--- src/ft_putchar.c
+++
@@ -1,6 +0,0 @@
-#include "header.h"
-void    ft_putchar(char symb)
-{
-    write(1, &symb, 1);
-    write(1, "\n", 1);
-}
